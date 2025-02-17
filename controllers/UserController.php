08088b9d4572aca2f1c0fd9196f84597e416c7fb
Denny Firmansyah <60263420+dennykunn@users.noreply.github.com>
2025-02-17 22:47:36 +0700

    Update UserController.php

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -3,34 +3,64 @@ session_start();
 require_once __DIR__ . "/../config/Database.php";
 require_once __DIR__ . "/../models/User.php";
 
-$db = new Database();
-$conn = $db->getConnection();
-$user = new User($conn);
+class UserController {
+    private $user;
+
+    public function __construct() {
+        $db = new Database();
+        $conn = $db->getConnection();
+        $this->user = new User($conn);
+    }
+
+    // Menambahkan pengguna baru
+    public function store() {
+        if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['store'])) {
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            $role_id = $_POST["role_id"];
+
+            if ($this->user->addUser($username, $password, $role_id)) {
+                $this->redirectWithMessage("Pengguna berhasil ditambahkan", "../index.php?page=users");
+            }
+        }
+    }
+
+    // Menghapus pengguna berdasarkan ID
+    public function delete() {
+        if (isset($_GET['delete'])) {
+            $this->user->deleteUser($_GET['delete']);
+            $this->redirectWithMessage("Pengguna berhasil dihapus", "../index.php?page=users");
+        }
+    }
+
+    // Memperbarui informasi pengguna
+    public function update() {
+        if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['update'])) {
+            $id = $_POST["id"];
+            $username = $_POST["username"];
+            $role_id = $_POST["role_id"];
+
+            if ($this->user->updateUser($id, $username, $role_id)) {
+                $this->redirectWithMessage("Pengguna berhasil diperbarui", "../index.php?page=users");
+            }
+        }
+    }
+
+    // Fungsi bantu untuk redirect dan menyimpan pesan session
+    private function redirectWithMessage($message, $location) {
+        $_SESSION['success'] = $message;
+        header("Location: $location");
+        exit();
+    }
+}
+
+// Inisialisasi controller dan jalankan metode yang sesuai
+$controller = new UserController();
 
 if (isset($_POST['store'])) {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $role_id = $_POST["role_id"];
-
-    if ($user->addUser($username, $password, $role_id)) {
-        $_SESSION['success'] = 'Pengguna berhasil ditambahkan';
-        header("Location: ../index.php?page=users");
-    }
-}
-
-if (isset($_GET['delete'])) {
-    $user->deleteUser($_GET['delete']);
-    $_SESSION['success'] = 'Pengguna berhasil dihapus';
-    header("Location: ../index.php?page=users");
-}
-
-if (isset($_POST['update'])) {
-    $id = $_POST["id"];
-    $username = $_POST["username"];
-    $role_id = $_POST["role_id"];
-
-    if ($user->updateUser($id, $username, $role_id)) {
-        $_SESSION['success'] = 'Pengguna berhasil diperbarui';
-        header("Location: ../index.php?page=users");
-    }
+    $controller->store();
+} elseif (isset($_GET['delete'])) {
+    $controller->delete();
+} elseif (isset($_POST['update'])) {
+    $controller->update();
 }
